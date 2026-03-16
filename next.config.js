8353a9ce2c49856fe23aac91e8fa7825795bfb5d
Zona Gilreath <36095717+zonagilreath@users.noreply.github.com>
2026-03-16 18:20:40 -0500

    feat: allow portfolio origins to embed via iframe

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOW-FROM https://zonadostuff.vercel.app https://zonadostuff-git-portfolio-rebuild-2026-zonafolio.vercel.app",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://zonadostuff.vercel.app https://zonadostuff-git-portfolio-rebuild-2026-zonafolio.vercel.app",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
